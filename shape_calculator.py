014134d1b060231d4e2f84a767689aeed06340f6
oejaramillo <oscar.10000@hotmail.com>
2023-04-03 15:45:36 -0300

    inicio

---
+++ shape_calculator.py
@@ -0,0 +1,23 @@
+class Rectangle:
+    def __init__(self, name):
+        self.name = name
+        self.width = 0
+        self.height = 0
+
+    def set_width(self, width):
+        self.width = width
+
+    def set_width(self, height):
+        self.height = height
+
+    def get_area(self):
+        area = self.width * self.height
+        return area
+    
+    
+
+    
+
+
+
+class Square:
