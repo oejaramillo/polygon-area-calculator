8a6b68ef61b80e5018363bc9eecfe852a9041725
oejaramillo <oscar.10000@hotmail.com>
2023-04-10 15:41:50 -0300

    final

--- shape_calculator.py
+++ shape_calculator.py
@@ -3,6 +3,9 @@ class Rectangle:
         self.width = width
         self.height = height
 
+    def __str__(self):
+        return f'Rectangle(width={self.width}, height={self.height})'
+
     def set_width(self, width):
         self.width = width
 
@@ -30,11 +33,21 @@ class Rectangle:
         else:
             return height_lines
         
+    def get_amount_inside(self, other_figure):
+        height_amount = self.height / other_figure.height
+        width_amount = self.width / other_figure.width
+
+        amount = height_amount * width_amount
+        return amount
+        
     
 class Square(Rectangle):
     def __init__(self, side):
         self.height = side
         self.width = side
+
+    def __str__(self):
+        return f'Square(side={self.height})'
 
     def set_side(self, side):
         self.height = side
@@ -42,21 +55,24 @@ class Square(Rectangle):
 
     def set_height(self, height):
         self.height = height
-        self.width = height
+        self.width = self.height
 
     def set_width(self, width):
         self.height = width
-        self.height = width
+        self.width = self.height
         
 
     
 
 rect = Rectangle(10, 5)
-rect.set_height(8)
-rect.set_width(4)
+rect.set_height(7)
+rect.set_width(9)
 
-rect2 = Rectangle(4, 4)
+rect2 = Rectangle(4, 8)
 
 sq1 = Square(8)
+sq1.set_height(7)
+sq1.set_width(4)
+sq1.set_side(4)
 
-print(rect.get_picture())
+print(rect.get_amount_inside(sq1))
